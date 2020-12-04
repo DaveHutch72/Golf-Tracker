3abb53bc296d085d206dbdc528c970c0e351bda8
David <dhutchis68516@gmail.com>
2020-12-04 10:08:00 -0600

    create reg contr, updated callback contr, updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
  
   resources :courses
   resources :rounds
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
   root to: 'application#welcome'
+
+  devise_scope :user do
+    get 'login', to:'devise/sessions#new', as:'login'
+    get 'signup', to: 'devise/registrations#new', as:'signup'
+  end
 end
